a415893c009542220578b155ec64e60b4e39227f
bluejynz <danifalk27@gmail.com>
2022-01-10 15:23:43 -0300

    ts config e auto update

--- api-authentication/src/index.ts
+++ api-authentication/src/index.ts
@@ -1,1 +1,11 @@
-console.log("oi");
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('App executando na porta 3000');
+});
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'Funfou!!'});
+});
