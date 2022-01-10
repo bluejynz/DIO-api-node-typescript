b41398c306587033b6d99fb46d26df2731ce26fd
bluejynz <danifalk27@gmail.com>
2022-01-10 16:58:54 -0300

    users crud implemented

--- api-authentication/src/index.ts
+++ api-authentication/src/index.ts
@@ -1,11 +1,15 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log('App executando na porta 3000');
 });
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Funfou!!'});
-});
+app.use(usersRoute);
+app.use(statusRoute);
