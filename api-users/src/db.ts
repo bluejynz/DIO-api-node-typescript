43c6cd478368c1d9d46d8685cfa15f00d274967b
Dany Falk <66040089+bluejynz@users.noreply.github.com>
2022-01-11 13:18:53 -0300

    Update db.ts

--- api-users/src/db.ts
+++ api-users/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://kugufkwj:hjhwlqfXtChsz1IJ9CRFV4svUSdZssd5@kesavan.db.elephantsql.com/kugufkwj';
+const connectionString = '???';
 const db = new Pool({ connectionString });
 
 export default db;
